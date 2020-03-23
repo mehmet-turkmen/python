4fa91b78055fe553af740d101867a331ee70df22
mehmet-turkmen <62249771+mehmet-turkmen@users.noreply.github.com>
2020-03-23 23:51:36 +0300

    Add files via upload

---
+++ Bölüm 4- Pythonda Operatörler/29. Bölüm Hakkında.py
@@ -0,0 +1,11 @@
+# Bölüm Hakkında
+# Artık Python' da kullanabilecek olduğumuz veri yapılarını öğrendik. 
+# Artık yavaş yavaş koşul içeren programlar hazırlamaya başlamak üzereyiz.
+
+# Bu bölümde tanımladığımız veri tiplerine değer ataması yaparken kullanacak
+#  olduğumuz Atama Operatörlerini, birden fazla veri yapısı içindeki değerlerin
+#   eşitlik, büyüklük vb. karşılaştırmalarını yapabilmek için gereken Karşılaştırma
+#    Operatörlerini ve birden fazla koşula bağlı ifadeleri tanımlayabilmek için
+#     gerekli olan Mantıksal Operatörleri öğreneceğiz.
+
+# İyi dersler diliyorum :)
