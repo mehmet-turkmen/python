196f41598053108db15d310f6fc97c6c0b30a553
mehmet-turkmen <62249771+mehmet-turkmen@users.noreply.github.com>
2020-03-23 23:53:35 +0300

    Add files via upload

---
+++ Bölüm 5- Pythonda Koşul İfadeleri/37. Bölüm Hakkında.py
@@ -0,0 +1,12 @@
+"""
+Bölüm Hakkında
+Artık operatörleri öğrendik ve istediğimiz şekilde koşul 
+hazırlayabilmek için gereken alt yapıya sahibiz. Özellikle 
+karşılaştırma sonucunda bize gelen boolean tipinin ne olduğunu artık biliyoruz.
+
+Bu bölümde kullanıcıdan aldığımız bir parolanın ya da email 
+adresinin doğru olup olmadığını kontrol edebileceğiz. Ya da 3
+ sayıdan hangisi büyüktür şeklinde bir karşılaştırmayı kolaylıkla 
+ yapıp geriye True ya da False değer döndürmek yerine
+  "en büyük sayı budur" şeklinde mesaj verebileceğiz.
+"""
